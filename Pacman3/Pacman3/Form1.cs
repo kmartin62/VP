786936e1e1d20006795e8642456c29c5afeac5d4
kmartin62 <martin.kostadinov@hotmail.com>
2018-05-08 18:12:07 +0200

    Pacman added

---
+++ Pacman3/Pacman3/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Pacman3.Properties;
+
+namespace Pacman3
+{
+    public partial class Form1 : Form
+    {
+
+        Pacman pacman;
+        Timer timer;
+        int TIMER_INTERVAL = 500;
+        Image foodImage = Resources.orange;
+        static readonly int WORLD_WIDTH = 15;
+        static readonly int WORLD_HEIGHT = 10;
+        bool[][] foodWorld;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+
+            newGame();
+        }
+
+        private void newGame()
+        {
+            pacman = new Pacman();
+            foodWorld = new bool[WORLD_HEIGHT][];
+            for (int i = 0; i < foodWorld.Length; i++)
+            {
+                foodWorld[i] = new bool[WORLD_HEIGHT];
+                for (int j = 0; j < foodWorld[i].Length; j++)
+                {
+                    foodWorld[i][j] = true;
+                }
+            }
+
+            timer = new Timer();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = TIMER_INTERVAL;
+            timer.Start();
+            this.Width = pacman.RADIUS * 2 * (WORLD_WIDTH + 1) - 25;
+            this.Height = pacman.RADIUS * 2 * (WORLD_HEIGHT + 1);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            for(int i = 0; i < foodWorld.Length; i++)
+            {
+                for(int j = 0; j < foodWorld[i].Length; j++)
+                {
+                    if(pacman.X == j && pacman.Y == i)
+                    {
+                        foodWorld[i][j] = false;
+                    }
+                }
+                
+            }
+            pacman.Move(WORLD_WIDTH, WORLD_HEIGHT);
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+            for (int i = 0; i < foodWorld.Length; i++)
+            {
+                for (int j = 0; j < foodWorld[i].Length; j++)
+                {
+                    if (foodWorld[i][j])
+                    {
+                        // g.DrawImageUnscaled(foodImage, j * pacman.RADIUS * 3 + (pacman.RADIUS * 2 - foodImage.Height) / 2, i * pacman.RADIUS * 2 + (pacman.RADIUS * 2 - foodImage.Width) / 2);
+                        g.DrawImageUnscaled(foodImage, j*pacman.RADIUS*3, i*pacman.RADIUS*2);
+                    }
+                }
+            }
+            pacman.Draw(g);
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up)
+            {
+                pacman.ChangeDirection(Pacman.DIRECTION.UP);
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                pacman.ChangeDirection(Pacman.DIRECTION.DOWN);
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                pacman.ChangeDirection(Pacman.DIRECTION.LEFT);
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                pacman.ChangeDirection(Pacman.DIRECTION.RIGHT);
+            }
+
+            Invalidate();
+        }
+    }
+}
---
+++ Pacman3/Pacman3/Pacman.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacman3
+{
+    public class Pacman
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public enum DIRECTION
+        {
+            UP, DOWN, LEFT, RIGHT
+        }
+        public DIRECTION direction;
+        public int RADIUS = 20;
+        public int Speed { get; set; }
+        public bool mouth { get; set; }
+
+        Brush b = new SolidBrush(Color.Yellow);
+
+        public Pacman()
+        {
+            Speed = RADIUS;
+            X = 5;
+            Y = 7;
+            direction = DIRECTION.DOWN;
+        }
+
+        public void ChangeDirection(DIRECTION dir)
+        {
+            direction = dir;
+        }
+
+        public void Move(int width, int height)
+        {
+            if(direction == DIRECTION.DOWN)
+            {
+                Y += 1;
+                if(Y >= height)
+                {
+                    Y = 0;
+                }
+            }
+
+            if (direction == DIRECTION.UP)
+            {
+                Y -= 1;
+                if (Y < 0)
+                {
+                    Y = height - 1;
+                }
+            }
+
+            if (direction == DIRECTION.RIGHT)
+            {
+                X += 1;
+                if (X >= width)
+                {
+                    X = 0;
+                }
+            }
+
+            if (direction == DIRECTION.LEFT)
+            {
+                X -= 1;
+                if (X < 0)
+                {
+                    X = height - 1;
+                }
+            }
+
+            mouth = !mouth;
+        }
+
+        public void Draw(Graphics g)
+        {
+            if (mouth)
+            {
+                g.DrawEllipse(new Pen(Color.Black), (RADIUS * 2 * X) + 115, RADIUS * 2 * Y, RADIUS * 2, RADIUS * 2);
+                g.FillEllipse(b, (RADIUS * 2 * X) + 115, RADIUS * 2 * Y, RADIUS * 2 , RADIUS * 2);
+            }
+            else
+            {
+                if(direction == DIRECTION.RIGHT)
+                {
+                    g.DrawPie(new Pen(Color.Black), (RADIUS * 2 * X) + 115, RADIUS * 2 * Y, RADIUS * 2, RADIUS * 2, 45, 270);
+                    g.FillPie(b, (RADIUS * 2 * X) + 115, RADIUS * 2 * Y, RADIUS * 2, RADIUS * 2, 45, 270);
+                }
+
+                if (direction == DIRECTION.DOWN)
+                {
+                    g.DrawPie(new Pen(Color.Black), (RADIUS * 2 * X) + 115, RADIUS * 2 * Y, RADIUS * 2, RADIUS * 2, 135, 270);
+                    g.FillPie(b, (RADIUS * 2 * X) + 115, RADIUS * 2 * Y, RADIUS * 2, RADIUS * 2, 135, 270);
+                }
+
+                if (direction == DIRECTION.LEFT)
+                {
+                    g.DrawPie(new Pen(Color.Black), (RADIUS * 2 * X) + 115, RADIUS * 2 * Y, RADIUS * 2, RADIUS * 2, 225, 270);
+                    g.FillPie(b, (RADIUS * 2 * X) + 115, RADIUS * 2 * Y, RADIUS * 2, RADIUS * 2, 225, 270);
+                }
+
+                if (direction == DIRECTION.UP)
+                {
+                    g.DrawPie(new Pen(Color.Black), (RADIUS * 2 * X) + 115, RADIUS * 2 * Y, RADIUS * 2, RADIUS * 2, 315, 270);
+                    g.FillPie(b, (RADIUS * 2 * X) + 115, RADIUS * 2 * Y, RADIUS * 2, RADIUS * 2, 315, 270);
+                }
+            }
+        }
+
+    }
+}
